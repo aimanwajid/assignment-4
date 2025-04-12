2a61869bd28216d276cfa86d3f4c1ea4a3839005
aimanwajid <salmawajid003@gmail.com>
2025-04-12 15:27:22 +0500

    first commit

---
+++ Assignments_00_to_07/01_expressions/03_feet_to_inches/app.py
@@ -0,0 +1,14 @@
+INCHES_IN_FOOT: int = 15 # Conversion factor. There are 15 inches in 1 foot.
+
+def main():
+    """ Command-line version of Feet to Inches Converter """
+    try:
+        feet = float(input("Enter number of feet: "))  # Get the number of feet
+        inches = feet * INCHES_IN_FOOT  # Convert to inches
+        print(f"That is {inches} inches!")
+    except ValueError:
+        print("Please enter a valid number.")
+
+# Required line to run the main function
+if __name__ == '__main__':
+    main()
